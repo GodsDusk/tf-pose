1a3913d59e4db86234bd0585c0a3d555c518e615
GodsDusk <197795352@qq.com>
2017-10-11 10:19:09 +0800

    beta

---
+++ data.m
@@ -0,0 +1,31 @@
+clc;clear
+load /home/xujunjie/work/convolutional-pose-machines-release/dataset/MPI/mpii_human_pose_v1_u12_1.mat
+
+annolist = RELEASE.annolist;
+train_num = find(RELEASE.img_train==1);
+path = '/MPI/images/';
+fid = fopen('data.txt', 'w');
+
+for i = 1:length(train_num)
+    try
+        index = train_num(i);
+        image = [path annolist(index).image.name];
+        annorect = annolist(index).annorect;
+        imsize = size(imread(image));
+        for j = 1:length(annorect)
+            point = annorect(j).annopoints.point;
+            objpos = annorect(j).objpos;
+            joints = zeros(16,2) - 1;
+            for k = 1:length(point)
+                id = point(k).id;
+                joints(id+1,:) = [point(k).x point(k).y];
+                
+            end
+            joints = reshape(joints', 1, []);
+            fprintf(fid, '%s\t%d %d %g %g %s\n', image, imsize(1), imsize(2), objpos.x, objpos.y, num2str(joints));
+        end
+    catch
+        continue
+    end
+end
+fclose(fid);
